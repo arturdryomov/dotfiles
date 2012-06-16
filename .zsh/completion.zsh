c491365eca915578aa6f32e4b9200e8bcfee42d7
Artur Dryomov <me@ming13.ru>
2012-06-16 16:10:47 +0300

    Remove some completion options and describe others.

--- .zsh/completion.zsh
+++ .zsh/completion.zsh
@@ -1,12 +1,11 @@
-zstyle ':completion:*' completer _expand _complete _ignored
-zstyle ':completion:*' group-name ''
-zstyle ':completion:*' list-prompt '%SAt %p: Hit TAB for more, or the character to insert%s'
-zstyle ':completion:*' max-errors 1
-zstyle ':completion:*' menu select=long-list select=0
-zstyle ':completion:*' select-prompt '%SScrolling active: current selection at %p%s'
-zstyle ':completion:*' use-compctl false
-zstyle ':completion:*' verbose true
-zstyle ':completion:*:descriptions' format "$fg_bold[brown] %B%d%b $reset_color"
+# Separate completion commands to groups with names-tags
+zstyle ":completion:*" group-name ""
+
+# Activate selecting command
+zstyle ":completion:*" menu select=long-list select=0
+
+# Pretty completion with command groups
+zstyle ":completion:*:descriptions" format "$fg_bold[brown] %B%d%b $reset_color"
 
 # Case insensitive completion for lowercase
 zstyle ":completion:*" matcher-list "m:{a-z}={A-Z}"
