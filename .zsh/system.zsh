3b78b7763b357ac9d30df547a36738b9054236e6
Artur Dryomov <artur.dryomov@gmail.com>
2012-09-08 19:21:53 +0300

    Add virtualenvwrapper default loading.

--- .zsh/system.zsh
+++ .zsh/system.zsh
@@ -4,5 +4,8 @@ export EDITOR="vim"
 # Python config
 export PYTHONSTARTUP=/home/ming/.pystartup
 
+# Enable virtualenvwrappert
+source virtualenvwrapper.sh
+
 # Classic time format
 export TIMEFMT=$'\nreal\t%E\nuser\t%U\nsys\t%S'
