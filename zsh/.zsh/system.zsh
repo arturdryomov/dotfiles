34643bb35bec91a93e2f581b47d626485a63ce97
Artur Dryomov <artur.dryomov@gmail.com>
2012-12-28 14:52:20 +0300

    Use lazy virtualenvwrapper loading.

--- zsh/.zsh/system.zsh
+++ zsh/.zsh/system.zsh
@@ -4,8 +4,8 @@ export EDITOR="vim"
 # Python config
 export PYTHONSTARTUP=/home/ming/.pystartup
 
-# Enable virtualenvwrappert
-source virtualenvwrapper.sh
+# Enable virtualenvwrapper
+source virtualenvwrapper_lazy.sh
 
 # Classic time format
 export TIMEFMT=$'\nreal\t%E\nuser\t%U\nsys\t%S'
