0ceffdf7b94d2ee80b267df63571cf71368c4a5e
Artur Dryomov <artur.dryomov@gmail.com>
2013-11-03 17:55:02 +0300

    Remove fancy stuff from the Python config.

--- python/.pythonrc.py
+++ python/.pythonrc.py
@@ -1,29 +1,15 @@
 import atexit
 import os
 import readline
-import rlcompleter
-import sys
 
 
 HISTORY_FILE = ".python_history"
 HISTORY_PATH = os.path.join(os.environ["HOME"], HISTORY_FILE)
 
 
-def setup_prompts():
-    sys.ps1 = build_prompt(">>>")
-    sys.ps2 = build_prompt("...")
-
-
-def build_prompt(prompt_text):
-    return "{bold_text} ".format(bold_text=build_bold_text(prompt_text))
-
-
-def build_bold_text(text):
-    return "\033[1m{bold_text}\033[0m".format(bold_text=text)
-
-
-def change_autocomplete_key_to_tab():
-    readline.parse_and_bind("tab: complete")
+def main():
+    read_history()
+    setup_saving_history()
 
 
 def read_history():
@@ -39,14 +25,5 @@ def save_history():
     readline.write_history_file(HISTORY_PATH)
 
 
-def main():
-    setup_prompts()
-
-    change_autocomplete_key_to_tab()
-
-    read_history()
-    setup_saving_history()
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
