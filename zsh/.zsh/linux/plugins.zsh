2c79d8cdfb51dbbb744bf54004e010a3e98378b1
Artur Dryomov <artur.dryomov@gmail.com>
2014-04-13 03:26:29 +0300

    Configure Linux Zsh plugins properly.

---
+++ zsh/.zsh/linux/plugins.zsh
@@ -0,0 +1,7 @@
+# Completion
+
+fpath=(/usr/share/zsh/site-functions $fpath)
+
+# Syntax highlighting
+
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
--- zsh/.zsh/unix/color-syntax.zsh
+++ zsh/.zsh/unix/color-syntax.zsh
@@ -2,7 +2,7 @@
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
 
 # Do not underline paths
-ZSH_HIGHLIGHT_STYLES[path]=none
+ZSH_HIGHLIGHT_STYLES[path]="none"
 
 # Yellow patterns
 ZSH_HIGHLIGHT_STYLES[globbing]="fg=yellow"
