8d17c7540df6278ac737fb74628760d203484ac8
Artur Dryomov <artur.dryomov@gmail.com>
2015-03-19 10:30:35 +0300

    Change Python configuration.

--- python/.pythonrc.py
+++ python/.pythonrc.py
@@ -8,21 +8,17 @@ HISTORY_PATH = os.path.join(os.environ["HOME"], HISTORY_FILE)
 
 
 def main():
-    load_history()
+    open_history()
     save_history()
 
 
-def load_history():
+def open_history():
     if os.path.exists(HISTORY_PATH):
         readline.read_history_file(HISTORY_PATH)
 
 
 def save_history():
-    atexit.register(write_history)
-
-
-def write_history():
-    readline.write_history_file(HISTORY_PATH)
+    atexit.register(lambda: readline.write_history_file(HISTORY_PATH))
 
 
 if __name__ == "__main__":
