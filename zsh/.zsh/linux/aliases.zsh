fe33b9aafccd4cea83791f0e5380e8ddcef32c89
Artur Dryomov <artur.dryomov@gmail.com>
2015-04-15 17:20:51 +0300

    Cleanup Linux Zsh aliases.

--- zsh/.zsh/linux/aliases.zsh
+++ zsh/.zsh/linux/aliases.zsh
@@ -7,7 +7,6 @@ alias y="yaourt"
 
 alias pkg-install="yaourt -S"
 alias pkg-uninstall="yaourt -Rns"
-
 alias pkg-search="yaourt -Ss"
 alias pkg-update="yaourt -Sy"
 alias pkg-upgrade="yaourt -Syu --aur"
@@ -20,28 +19,7 @@ alias hide="systemctl suspend"
 
 ## Other
 
-alias translate="sdcv"
+alias open="xdg-open"
 
 alias pbcopy="xsel --clipboard --input"
 alias pbpaste="xsel --clipboard --output"
-
-alias open="xdg-open"
-
-
-# Specific for hosts
-
-if [ ${HOST} = "desktop" ]; then
-  alias traffic="vnstat -i eth0 -m"
-  alias speed="slurm -i eth0"
-fi
-
-if [ ${HOST} = "netbook" ]; then
-  alias traffic="vnstat -i wlan0 -m"
-  alias speed="slurm -i wlan0"
-
-  alias wifi="wicd-curses"
-
-  alias make-right-screen="xrandr --verbose --output VGA1 --auto --right-of LVDS1"
-  alias make-double-screen="xrandr --output VGA1 --auto --output LVDS1 --mode 1024x600"
-  alias remove-external-screen="xrandr --output VGA1 --off"
-fi
