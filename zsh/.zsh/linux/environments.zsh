d47fb496a820abc7e3340f732479641025a793e8
Artur Dryomov <artur.dryomov@gmail.com>
2015-07-29 13:19:34 +0300

    Add Python environments support for Linux.

--- zsh/.zsh/linux/environments.zsh
+++ zsh/.zsh/linux/environments.zsh
@@ -6,3 +6,11 @@ export PATH="${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools"
 # Maven
 
 export MAVEN_HOME="/opt/maven/"
+
+# Python environments
+
+if [[ -f "/usr/bin/virtualenvwrapper.sh" ]]; then
+  export WORKON_HOME="${HOME}/.python_virtualenvs"
+
+  source "/usr/bin/virtualenvwrapper_lazy.sh"
+fi
