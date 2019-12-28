0525c2dfbb4573cc65cf0c7c47d727c0c38217b7
Artur Dryomov <artur.dryomov@gmail.com>
2019-12-28 18:39:09 +0300

    Remove unused Zsh scripts.

--- zsh/.zsh/darwin/environments.zsh
+++ zsh/.zsh/darwin/environments.zsh
@@ -11,20 +11,3 @@ export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
 export ANDROID_HOME="${BREW_HOME}/share/android-sdk/"
 
-# Maven
-
-export MAVEN_HOME="${BREW_HOME}/opt/maven/"
-
-# Python environments
-
-if [[ -f "${BREW_HOME}/bin/virtualenvwrapper.sh" ]]; then
-  export WORKON_HOME="${HOME}/.python_virtualenvs"
-
-  source "${BREW_HOME}/bin/virtualenvwrapper_lazy.sh"
-fi
-
-# Ruby environments
-
-if type "rbenv" > /dev/null; then
-  eval "$(rbenv init - --no-rehash zsh)"
-fi
