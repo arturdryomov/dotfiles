b9b51fac987ebf986ea654b738384c04a436176f
Artur Dryomov <artur.dryomov@gmail.com>
2026-01-14 18:26:59 +0100

    Remove unused Zsh Linux aliases

--- zsh/.zsh/linux/aliases.zsh
+++ zsh/.zsh/linux/aliases.zsh
@@ -1,23 +1,3 @@
-# Common
-
-## Packages
-
-alias p="pacman"
-alias y="yaourt"
-
-alias pkg-install="yaourt --sync"
-alias pkg-uninstall="yaourt --remove --recursive --nosave"
-alias pkg-search="yaourt --search --sync"
-alias pkg-upgrade="yaourt --sysupgrade --sync --refresh --aur"
-
-## System
-
-alias off="systemctl poweroff"
-alias reboot="systemctl reboot"
-alias hide="systemctl suspend"
-
-## Integrations
-
 alias open="xdg-open"
 
 alias pbcopy="xsel --clipboard --input"
