5faa8d70bb0205cd1d4193f5087626466114670f
Artur Dryomov <artur.dryomov@gmail.com>
2026-01-18 08:26:23 +0100

    Add XDG environment variables for macOS

--- zsh/.zsh/darwin/_environment.zsh
+++ zsh/.zsh/darwin/_environment.zsh
@@ -1,3 +1,9 @@
 # Homebrew
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# XDG
+
+export XDG_CACHE_HOME="${HOME}/.cache"
+export XDG_CONFIG_HOME="${HOME}/.config"
+export XDG_DATA_HOME="${HOME}/.local"
