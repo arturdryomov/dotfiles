4c10833e022eeb37211b251c9222062413009247
Artur Dryomov <artur.dryomov@gmail.com>
2026-01-19 18:47:48 +0100

    Change macOS XDG paths

--- zsh/.zsh/darwin/_environment.zsh
+++ zsh/.zsh/darwin/_environment.zsh
@@ -6,4 +6,5 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 
 export XDG_CACHE_HOME="${HOME}/.cache"
 export XDG_CONFIG_HOME="${HOME}/.config"
-export XDG_DATA_HOME="${HOME}/.local"
+export XDG_DATA_HOME="${HOME}/.local/share"
+export XDG_STATE_HOME="${HOME}/.local/state"
