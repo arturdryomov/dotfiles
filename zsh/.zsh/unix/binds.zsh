21e6587ee65215977a4264586aee9c6808a445f8
Artur Dryomov <artur.dryomov@gmail.com>
2026-01-24 19:27:16 +0100

    Change Zsh word navigation

--- zsh/.zsh/unix/binds.zsh
+++ zsh/.zsh/unix/binds.zsh
@@ -3,3 +3,6 @@ bindkey -e
 
 # Avoid removing all text with ^U (default is "kill-whole-line")
 bindkey "^U" backward-kill-line
+
+# Change word navigation to recognize "/", "." and friends
+autoload -Uz select-word-style; select-word-style bash
